d6a5119ee5c81b668ce32f8402b249636b18b3f6
Camilo Flores <camilo.ignacio@gmail.com>
2015-05-08 16:20:23 -0300

    - Removes tracking of temp files (index.txt, serial) - Signing scripts creates temp files on-demand

--- sign_csr.sh
+++ sign_csr.sh
@@ -5,6 +5,14 @@ then
 	echo "Usage: sign_csr <APP_NAME>"
 	echo "Example: sign_csr myapp"
 else
+	if ! [[ -e "index.txt" ]]
+	then
+		touch index.txt
+	fi
+	if ! [[ -e serial ]]
+	then
+		echo "01" > serial
+	fi
 	openssl ca \
 		-out ./out/${1}.crt -config ./openssl.cnf \
 		-infiles ./out/${1}.csr
